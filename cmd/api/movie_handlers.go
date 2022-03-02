311b9adcca6919bd05b9ed6a0d89b3a6450d436b
lkaing <lkaing@openwt.com>
2022-03-02 08:51:06 +0100

    init project

---
+++ cmd/api/movie_handlers.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"backend/models"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameters"))
+		app.errorJSON(w, err)
+		return
+	}
+
+	movie, err := app.models.DB.Get(id)
+
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+}
+
+func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
+	genres, err := app.models.DB.GenresAll()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, genres, "genres")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	genreID, err := strconv.Atoi(params.ByName("genre_id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	movies, err := app.models.DB.All(genreID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+type MoviePayload struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Year        string `json:"year"`
+	ReleaseDate string `json:"release_date"`
+	Runtime     string `json:"runtime"`
+	Rating      string `json:"rating"`
+}
+
+func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
+	var movie models.Movie
+
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println(movie.Title)
+
+	type jsonResp struct {
+		OK bool `json:"ok"`
+	}
+
+	ok := jsonResp{
+		OK: true,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, ok, "response")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ cmd/api/routes.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+	// secure := alice.New(app.checkToken)
+
+	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/signin", app.Signin)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
+
+	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
+
+	// router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editMovie)))
+
+	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+
+	return app.enableCORS(router)
+}
